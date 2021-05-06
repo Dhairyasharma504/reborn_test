ab459d898f460d53c8eeb1d92f950e3e8631ef49
dhairyasharma504 <dhairyasharma2311@gmail.com>
2021-05-06 11:31:03 -0700

    first commit

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {Text, View, StyleSheet} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.image}>htjr</Text>
+      <Text style={styles.image}>htuujr</Text>
     </View>
   );
 };
