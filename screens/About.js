20348d82141388d75f5a295db34d1ed8f59f8855
dhairyasharma504 <dhairyasharma2311@gmail.com>
2021-05-17 21:43:06 -0700

    minor fix

--- screens/About.js
+++ screens/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text,Button} from 'react-native';
 
 const About = () => {
   return (
     <View style={styles.center}>
       <Text>This is the about screen</Text>
+      <Button
+        title="Go to About Screen"
+        onPress={() => navigation.navigate("About")} // We added an onPress event which would navigate to the About screen
+      />
     </View>
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Button, Text, StyleSheet } from "react-native";
 const Home = ({ navigation }) => {
   return (
     <View style={styles.center}>
-      <Text>This is the home screen</Text>
+      <Text>Go to About Screen</Text>
       <Button
         title="Go to About Screen"
         onPress={() => navigation.navigate("About")} // We added an onPress event which would navigate to the About screen
@@ -17,7 +17,8 @@ const styles = StyleSheet.create({
   center: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#dcfffd",
+    alignItems: "left",
     textAlign: "center",
   },
 });
