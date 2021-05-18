a573f24755b4745f362e3e652015a044fa1f10d4
dhairyasharma504 <dhairyasharma2311@gmail.com>
2021-05-18 00:22:54 -0700

    changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
+import Button_square from "../components/Button_square";
 
 const Home = ({ navigation }) => {
   return (
     <View style={styles.center}>
       <Text>Go to About Screen</Text>
+
+      <Button_square
+      square_button="ffvfgb"
+      />
       <Button
         title="Go to About Screen"
         onPress={() => navigation.navigate("About")} // We added an onPress event which would navigate to the About screen
@@ -18,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#dcfffd",
-    alignItems: "left",
+
     textAlign: "center",
   },
 });
